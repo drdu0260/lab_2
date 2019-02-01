1e4768e91e36cf6d646862e57ea616ca8238e637
drdu0260 <drdu0260@colorado.edu>
2019-01-31 21:25:07 -0700

    Practice.sh git commands

---
+++ practice.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author : Your Name
+# Date: 1/1/2019
+# Script follows here:
+#!/bin/bash
+# Author : Your Name
+# Date: 1/1/2019
+# Script follows here:
+echo "File Name: $0"
+echo "Command Line Arguement: $1"
+grep $1 $2
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "The sum is : $sum"
+let prod=numOne*numTwo
+echo "The product is: $prod"
