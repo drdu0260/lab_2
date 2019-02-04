0c0143ee0f65092d288bbf21d7895957973113ac
drdu0260 <drdu0260@colorado.edu>
2019-02-03 18:38:07 -0700

    Creating practice.sh file & storing regex_practice.sh

--- practice.sh
+++ practice.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# Author : Your Name
-# Date: 1/1/2019
-# Script follows here:
-#!/bin/bash
-# Author : Your Name
-# Date: 1/1/2019
+# Author : Drew Dugan
+# Date: 1/31/2019
 # Script follows here:
 echo "File Name: $0"
 echo "Command Line Arguement: $1"
